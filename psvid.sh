fce964998519f86d61c0148dca71feaf95c3acb3
Roberto Santalla <roobre@roobre.es>
2018-05-23 18:04:47 +0200

    Added mp4 streaming flags, removed vp8, scale to 720

--- psvid.sh
+++ psvid.sh
@@ -1,34 +1,31 @@
 #!/bin/bash
 
-MP4FLAGS="-c:a aac -b:a 192k -c:v libx264 -preset slower -movflags empty_moov+default_base_moof+frag_keyframe"
-WEBMFLAGS="-c:a libvorbis"
+COMMONFLAGS="-r 30 -vf scale=-1:720"
+MP4FLAGS="-c:a aac -b:a 192k -c:v libx264 -preset veryslow -tune film -movflags empty_moov+default_base_moof+frag_keyframe"
+WEBMFLAGS="-speed 1 -threads 4 -keyint_min 150 -g 150 -tile-columns 4 -frame-parallel 1 -f webm -c:a libvorbis"
 
 FFMPEGFLAGS=(\
-    "$MP4FLAGS -crf 20 -profile:v main     -level 3.1 "\
-    "$MP4FLAGS -crf 22 -profile:v baseline -level 3.1"\
-    "-c:v libvpx-vp9 -crf 31 -b:v 0  $WEBMFLAGS"\
-    "-c:v libvpx     -crf 10 -b:v 4M $WEBMFLAGS"\
+    "$MP4FLAGS -crf 23 -profile:v main     -level 3.1"\
+    "$MP4FLAGS -crf 24 -profile:v baseline -level 3.1"\
+    "-c:v libvpx-vp9 -crf 34 -b:v 0  $WEBMFLAGS"\
 )
 
 EXTENSIONS=(\
     ".main.mp4"\
     ".baseline.mp4"\
     ".vp9.webm"\
-    ".vp8.webm"\
 )
 
 MIMES=(\
     'video/mp4; codecs="avc1.4D401E, mp4a.40.2"'\ # Main 3.X
     'video/mp4; codecs="avc1.42E01E, mp4a.40.2"'\ # Baseline 3.X
     'video/webm; codecs="vp9, vorbis"'\
-    'video/webm; codecs="vp8, vorbis"'\
 )
 
 PRIORITIES=(\
-    1300\
     1200\
     1100\
-    1000\
+    1300\
 )
 
 if [ -z $1 ]; then
@@ -49,7 +46,7 @@ mkdir -p $outfolder
 echo -n '' > $outfolder/mimes.txt
 
 for (( i = 0; i < ${#FFMPEGFLAGS[@]}; i++ )); do
-    ffmpeg -i "$1" ${FFMPEGFLAGS[$i]} $outfolder/$outfolder${EXTENSIONS[$i]}
+    ffmpeg -i "$1" $COMMONFLAGS ${FFMPEGFLAGS[$i]} $outfolder/$outfolder${EXTENSIONS[$i]}
     echo "${EXTENSIONS[$i]}: ${MIMES[$i]}" >> $outfolder/mimes.txt
 done
 
